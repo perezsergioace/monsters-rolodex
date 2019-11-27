7a052eb289f34c195ca73fda0971516d66d0ed89
perezsergioace <perez.sergio.ace@gmail.com>
2019-11-26 19:02:48 -0500

    Updates

---
+++ precache-manifest.8bed1e44790b55f49ec6fbd29bf4c930.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29e6adaf4c01ded7401d0eff4a7b95ae",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "d251c8be9e21bb86e65e",
+    "url": "/monsters-rolodex/static/css/main.e5047e4f.chunk.css"
+  },
+  {
+    "revision": "3a057e02db196afa0ca3",
+    "url": "/monsters-rolodex/static/js/2.f833121f.chunk.js"
+  },
+  {
+    "revision": "d251c8be9e21bb86e65e",
+    "url": "/monsters-rolodex/static/js/main.436b8617.chunk.js"
+  },
+  {
+    "revision": "2dc9b40a277f725e4d87",
+    "url": "/monsters-rolodex/static/js/runtime-main.b05ca383.js"
+  }
+]);
